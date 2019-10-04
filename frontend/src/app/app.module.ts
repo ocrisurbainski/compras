2dc6230264c8f598a4d1bc5997828ba4a87b639f
Cristian Urbainski <cristianurbainskips@gmail.com>
2019-10-03 23:49:25 -0300

    Novos componentes, pipes, e novas importações necessárias ao projeto.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +12,13 @@ import { DefaultIndexComponent } from './views/default-index/default-index.compo
 import { ListaComprasComponent } from './views/compras/lista-compras/lista-compras.component';
 import { NovaCompraComponent } from './views/compras/nova-compra/nova-compra.component';
 import { PageTitleComponent } from './componente/page-title/page-title.component';
+import { CnpjCpfPipe } from './pipe/cnpj-cpf.pipe';
+import { CepPipe } from './pipe/cep.pipe';
+
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { ModalModule } from 'ngx-bootstrap';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalCompraProdutosComponent } from './views/compras/lista-compras/modal-compra-produtos/modal-compra-produtos.component';
 
 @NgModule({
 	declarations: [
@@ -20,11 +29,24 @@ import { PageTitleComponent } from './componente/page-title/page-title.component
 		DefaultIndexComponent,
 		ListaComprasComponent,
 		NovaCompraComponent,
-		PageTitleComponent
+		PageTitleComponent,
+		CnpjCpfPipe,
+		CepPipe,
+		ModalCompraProdutosComponent
 	],
 	imports: [
 		BrowserModule,
-		AppRoutingModule
+		AppRoutingModule,
+
+		HttpClientModule,
+		FormsModule,
+
+		PaginationModule.forRoot(),
+		ModalModule.forRoot(),
+		TooltipModule.forRoot()
+	],
+	entryComponents: [
+		ModalCompraProdutosComponent
 	],
 	providers: [],
 	bootstrap: [AppComponent]
