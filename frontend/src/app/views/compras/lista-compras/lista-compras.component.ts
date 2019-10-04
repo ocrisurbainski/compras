255d30c9b1252f7260b10d7734d9852b2f08fe8b
Cristian Urbainski <cristianurbainskips@gmail.com>
2019-10-03 23:48:34 -0300

    Implementado tela de listagem de compras.

--- frontend/src/app/views/compras/lista-compras/lista-compras.component.ts
+++ frontend/src/app/views/compras/lista-compras/lista-compras.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { CompraService } from 'src/app/service/compra.service';
+import { Compra } from 'src/app/entity/compra';
+import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap';
+import { ModalCompraProdutosComponent } from './modal-compra-produtos/modal-compra-produtos.component';
 
 @Component({
-  selector: 'app-lista-compras',
-  templateUrl: './lista-compras.component.html',
-  styleUrls: ['./lista-compras.component.css']
+	selector: 'app-lista-compras',
+	templateUrl: './lista-compras.component.html',
+	styleUrls: ['./lista-compras.component.css']
 })
 export class ListaComprasComponent implements OnInit {
 
-  constructor() { }
+	public totalItens : number = 0;
 
-  ngOnInit() {
-  }
+	public itemsPerPage : number = 20;
+
+	public currentPage : number = 1;
+
+	public compras : Compra[] = [];
+
+	private bsModalRef : BsModalRef;
+
+	constructor(
+		private compraService : CompraService,
+		private modalService : BsModalService) { }
+
+	ngOnInit() {
+		this.findAll(this.currentPage, this.itemsPerPage);
+	}
+
+	pageChanged(event : any) {
+		let page : number = event.page - 1;
+		this.findAll(page, this.itemsPerPage);
+	}
+
+	openModalProdutos(compra : Compra) {
+		let modalOptions = new ModalOptions();
+		modalOptions.class = 'modal-xl';
+		modalOptions.backdrop = 'static';
+		modalOptions.ignoreBackdropClick = true;
+		modalOptions.initialState = {
+			'produtos' : compra.produtos,
+			'class': 'modal-xl'
+		};
+		this.bsModalRef = this.modalService.show(ModalCompraProdutosComponent, modalOptions);
+	}
+
+	private findAll(page : number, size : number) {
+		this.compraService.findAll(page, size).subscribe((res) => {
+			this.compras = res['data'];
+			this.totalItens = res['total'];
+		})
+	}
 
 }
